78f7fd487fe59367999094c00f5d7bf470fccf14
Ada Null <ada@worldenddisk.com>
2024-07-23 18:37:13 +0100

    backend base url is an environment variable

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,7 +1,8 @@
 import type { PageServerLoad } from "./$types";
+import { env } from "$env/dynamic/private"
 
 export const load: PageServerLoad = async() => {
-    let response = await fetch('http://localhost:5555/');
+    let response = await fetch(env.BACKEND_BASE_URL);
     let shore = await response.json() as Shore;
         
     return {
