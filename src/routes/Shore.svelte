183081da17802c2d20e781c4fb372317f0fb006f
Ada Null <ada@worldenddisk.com>
2024-07-30 14:26:56 +0100

    Rendering one item with canvas api

--- src/routes/Shore.svelte
+++ src/routes/Shore.svelte
@@ -1,14 +1,22 @@
 <script lang="ts">
+    import { Canvas } from 'svelte-canvas';
     import ShoreItem from './ShoreItem.svelte';
 
     export let shore: Shore;
-    let splitContents = []
-    for (let i = 0; i < shore.contents.length; i += shore.width) {
-        splitContents.push(shore.contents.slice(i, i + shore.width));
-    }
+    // let splitContents = []
+    // for (let i = 0; i < shore.contents.length; i += shore.width) {
+    //     splitContents.push(shore.contents.slice(i, i + shore.width));
+    // }
+
+    let canvas: Canvas;
+    $: context = canvas?.getContext();
 </script>
 
-<div class="shore">
+<Canvas bind:this={canvas} width={1000} layerEvents>
+    <ShoreItem {context} item={shore.contents[0]} />
+</Canvas>
+
+<!-- <div class="shore">
     {#each splitContents as row}
         <div class="row" >
             {#each row as item}
@@ -16,18 +24,12 @@
             {/each}
         </div>
     {/each}
-</div>
+</div> -->
 
 
-<style>
+<!-- <style>
     .shore {
-        display: flex;
-        flex-direction: column;
         overflow-y: scroll;
         padding: 0 30px 0;
     }
-
-    .row {
-        display: flex;
-    }
-</style>
+</style> -->
--- src/routes/ShoreItem.svelte
+++ src/routes/ShoreItem.svelte
@@ -1,32 +1,45 @@
 <script lang="ts">
-    import { inventory } from "./stores";
+    import { Canvas, Layer, type Render } from 'svelte-canvas';
+    import { inventory } from './stores';
+    import { text } from '@sveltejs/kit';
 
+    export let context: CanvasRenderingContext2D;
     export let item: Item;
-
+    
+    let x = 5;
+    let y = 5;
     let collected = false;
 
     function collect() {
         if (collected == false) {
             collected = true;
-
             $inventory = [...$inventory, item];
+            let textMetrics = context.measureText(item.text);
+            context.clearRect(
+                x, 
+                y,
+                textMetrics.actualBoundingBoxRight,
+                textMetrics.actualBoundingBoxDescent
+            );
         }
+    }
+
+    let render: Render;
+    $: render = ({ context }) => {
+        context.font = '16px Times New Roman';
+        context.textAlign = 'left';
+        context.textBaseline = 'top';
+        context.fillStyle = 'black';
+        context.fillText(item.text, x, y);
+        let textMetrics = context.measureText(item.text);
+        context.fillStyle = 'transparent';
+        context.fillRect(
+            x, 
+            y,
+            textMetrics.actualBoundingBoxRight,
+            textMetrics.actualBoundingBoxDescent
+        );
     }
 </script>
 
-<button on:click={collect} class:hidden={collected} >{item.text.replaceAll(" ", "")}</button>
-
-<style>
-    .hidden {
-        visibility: hidden;
-    }
-
-    button {
-        margin: 0;
-        padding: 0;
-        background: none;
-        border: none;
-        font-family: serif;
-        font-size: medium;
-    }
-</style>
+<Layer {render} on:click={collect} />
